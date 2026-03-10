c7bfac3e242ab0dfc23a07ce1c6d3bccd42cd0b7
Caio Sousa <caiosousaap@gmail.com>
2026-03-10 20:38:52 -0300

    feat: AlunoRepositoryMemoria.java

--- src/repository/AlunoRepositoryMemoria.java
+++ src/repository/AlunoRepositoryMemoria.java
@@ -1,4 +1,33 @@
 package src.repository;
 
-public class AlunoRepositoryMemoria {
+import src.model.Aluno;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AlunoRepositoryMemoria implements AlunoRepository {
+
+    private final List<Aluno> alunos = new ArrayList<>();
+    private int contadorId = 0;
+
+    @Override
+    public void salvar(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+    @Override
+    public List<Aluno> listarTodos() {
+        return Collections.unmodifiableList(alunos);
+    }
+
+    @Override
+    public void deletarTodos() {
+        alunos.clear();
+        contadorId = 0;
+    }
+
+    @Override
+    public int proximoId() {
+        return contadorId++;
+    }
 }
